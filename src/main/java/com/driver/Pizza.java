3800dee0b0d56558fe03b71a9a8044b8d8ed3182
sowmya-pusapati <pusapatisowmya@gmail.com>
2023-01-20 22:16:54 +0530

    first committ

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,40 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int extraCheese;
+    private int extraToppings;
+
+    boolean isExtraCheese;
+    boolean isExtraToppings;
+    boolean isTakeway;
+    boolean isBillGenerated;
+
 
     public Pizza(Boolean isVeg){
+        this.isExtraCheese=false;
+        this.isExtraToppings=false;
+        this.isTakeway=false;
+        this.isBillGenerated=false;
         this.isVeg = isVeg;
-        // your code goes here
+        this.bill="";
+
+        if(isVeg) {
+            this.price = 300;
+            this.extraToppings = 70;
+        }
+        else {
+            this.price=400;
+            this.extraToppings = 120;
+        }
+
+        this.extraCheese=80;
+        this.bill+="Base Price Of the Pizza: "+this.price+"\n";
+    }
+
+
+
+    public void setBill(String bill) {
+        this.bill = bill;
     }
 
     public int getPrice(){
@@ -17,18 +47,48 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
+     if(!isExtraCheese)
+     {
+       this.price=price+extraCheese;
+       isExtraCheese=true;
+     }
+
+
     }
 
     public void addExtraToppings(){
         // your code goes here
+
+        if(!isExtraToppings)
+        {
+            this.price+=extraToppings;
+            isExtraToppings=true;
+        }
+
     }
 
     public void addTakeaway(){
-        // your code goes here
+        if(!isTakeway)
+        {
+            this.price+=20;
+            isTakeway=true;
+        }
+
     }
 
-    public String getBill(){
+    public String getBill() {
         // your code goes here
-        return this.bill;
+        if (!isBillGenerated) {
+            if (isExtraCheese)
+                this.bill += "Extra Cheese Added: " + this.extraCheese + "\n";
+            if (isExtraToppings)
+                this.bill += "Extra Toppings Added: " + this.extraToppings + "\n";
+            if (isTakeway)
+                this.bill += "Paper bag Added: " + "20" + "\n";
+
+            this.bill += "Total: " + this.price+"\n";
+        }
+            return this.bill;
+
     }
 }
